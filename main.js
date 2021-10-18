224aa504d14d19471a691f8f76e49ca446afd934
stefansnaerh <stefansnaerh@gmail.com>
2021-10-18 15:18:13 +0000

    made text change on startpage

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-// let h2 = document.querySelector('h2')
+
 // let classes = (classes) => document.getElementsByClassName (classes);
 
-// let breakfast = classes("backgroundImage1")
-// let lunch = classes("backgroundImage3")
-// let dinner = classes("backgroundImage2") 
+// let breakfast = classes("backgroundImage1"),
+//  lunch = classes("backgroundImage3"),
+//  dinner = classes("backgroundImage2"),
+//  greeting = classes("morning");
 
 // let time = new Date().getHours();
 // if (time < 1) {
@@ -20,6 +21,7 @@
 // 	lunch.style.opacity = 0;
 // 	h2.innerHTML = "What's for dinner?"
 // }
+// console.log(time)
 
 
 
@@ -29,3 +31,35 @@
 // if (currenTime < 10) {
 //     document.
 // };
+
+
+
+let title = document.querySelector("h2") 
+
+let time = new Date().getHours();
+if (time < 10) {
+title.innerHTML=("What's for breakfast?");
+} else if (time < 14) {
+    title.innerHTML=("What's for lunch?");
+} else {
+    title.innerHTML=("What's for dinner?")
+};
+ time();
+// let backgroundImage = document.getElementById("#morning")
+// let title = document.querySelector("h2") 
+
+// let time = new Date().getHours();
+// if (time < 23) {
+//     title.innerHTML=("What's for breakfast?");
+//     document.body.style.backgroundImage = "url('images/Startpage-Breakfast.png')"
+
+// } else if (time < 14) {
+//     title.innerHTML=("What's for lunch?");
+//     backgroundImage.innerHTML= `<img src="images/lunch.png" class="backgroundImageBreakfast" alt="" id="morning">`
+    
+// } else {
+//     title.innerHTML=("What's for dinner?");
+//     backgroundImage.innerHTML=`<img src="images/dinner.png" class="backgroundImageBreakfast" alt="" id="morning"></img>`
+    
+// };
+//  time();
