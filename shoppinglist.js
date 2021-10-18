ad2e98810b7bf05a2f02da5abd5f60f86963c817
Jakub Mierzejek <jakubmierzejek@MBP-Joanna.hringdu.is>
2021-10-18 15:13:08 +0000

    shoppinglist updated

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,30 +1,35 @@
-    // Add Additional Items
-    var aside = document.querySelector('aside'),
-        div = document.querySelector('div'),
-        text = document.querySelector('input'),
-        button = document.querySelector('button')
-    text.type = 'text'
-    text.id = 'input'
-    button.type = 'button'
-    button.setAttribute('data-button','outline')
-    button.setAttribute('onclick','addItem(input.value)')
-    div.appendChild(text)
-    div.appendChild(button)
-    aside.appendChild(div)
+document.querySelector("#push").onclick = function
+(){
+  if(document.querySelector("#newItem input").value.length == 0){
+    alert("please write item")
+  }
+  else{
+    document.querySelector("#items").innerHTML += `
+    <div class="item">
+      <span id="itemName">
+        ${document.querySelector ("#newItem input").value}
+      </span>
+      <button class="remove">
+        <i class="far fa-trash-alt"></i>
+      </button>
+    </div>  `;
 
-    function addItem(text){
-      var div = document.querySelector('div'),
-          input = document.getElementById('input')
-      if (text !== ''){
-        input.value = ''
-        var label = document.createElement('label'),
-            input = document.createElement('input'),
-            span = document.createElement('span')
-        input.type = 'checkbox'
-        span.innerHTML = text
-        label.appendChild(input)
-        label.appendChild(span)
-        div.parentNode.insertBefore(label,div)
+    let currentItem = document.querySelectorAll(".remove");
+    for(let i=0; i<currentItem.length; i++){
+      currentItem[i].onclick = function(){
+        this.parentNode.remove();
       }
     }
+
+    let items = document.querySelectorAll(".item");
+    for(let i=0; i<items.length; i++){
+      items[i].onclick = function(){
+        this.classList.toggle("completed");
+      }
+    }
+
+    document.querySelector("#newItem input").value = "";
+  }
+}
+
 
