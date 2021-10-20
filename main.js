4cabdf8ee963c359b5283837b4526a2b4ceceff1
stefansnaerh <stefansnaerh@gmail.com>
2021-10-20 10:12:36 +0000

    pushing

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+
+
+
+
+
+
+// Here I call out the H2 element i want to change
 let title = document.querySelector("h2") 
+
+
 
 let time = new Date().getHours();
 if (time < 10) {
 title.innerHTML=("What's for breakfast?");
 document.getElementById("morning").src="images/Startpage-Breakfast.png";
-} else if (time < 14) {
+} else if (time < 10) {
     title.innerHTML=("What's for lunch?");
 document.getElementById("morning").src="images/lunch.png";
 
