923cc435e0fcf094d70014d77b5889dec8b2dec8
Jakub Mierzejek <jakubmierzejek@MBP-Joanna.hringdu.is>
2021-10-21 10:15:24 +0000

    fixed shopping list and my feed

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,12 +1,10 @@
 const itemsDiv = document.querySelector("#items");
-console.log(itemsDiv)
 document.querySelector("#push").onclick = function
 (){
   if(document.querySelector("#newItem input").value.length == 0){
     alert("please write item")
   }
   else{
-    console.log(itemsDiv)
     itemsDiv.innerHTML += `
     <div class="item">
       <span id="itemName">
@@ -17,15 +15,18 @@ document.querySelector("#push").onclick = function
       </button>
     </div>  `;
     localStorage.setItem("items", itemsDiv.innerHTML)
-
-    let currentItem = document.querySelectorAll(".remove");
+    makeInteractive();
+    document.querySelector("#newItem input").value = "";
+  }
+}
+let makeInteractive = () => {
+  let currentItem = document.querySelectorAll(".remove");
     for(let i=0; i<currentItem.length; i++){
       currentItem[i].onclick = function(){
         this.parentNode.remove();
       }
-      localStorage.removeItem("items", itemsDiv.innerHTML)
+      localStorage.setItem("items", itemsDiv.innerHTML)
     }
-
     let items = document.querySelectorAll(".item");
     for(let i=0; i<items.length; i++){
       items[i].onclick = function(){
@@ -33,9 +34,8 @@ document.querySelector("#push").onclick = function
       }
       localStorage.setItem("items", itemsDiv.innerHTML)
     }
-
-    document.querySelector("#newItem input").value = "";
-  }
 }
 
-items.innerHTML = localStorage.getItem("items")
+itemsDiv.innerHTML = localStorage.getItem("items")
+makeInteractive();
+
